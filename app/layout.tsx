b1e2fb86e0b8efa6374869c8d654805b1cafd323
Vyvusstartup <vyvus.startup@gmail.com>
2025-09-15 07:43:45 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="Vyvus" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+
+        {/* Favicon / Icons */}
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16.png" />
+        <link rel="shortcut icon" href="/favicon.ico" />
       </head>
       <body>
         <I18nProvider>
@@ -54,6 +60,4 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     </html>
   );
 }
-
-
 
